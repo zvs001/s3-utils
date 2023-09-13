5a173100b9c1c4cdff7d03210e68fd21d1d0d7d8
zvsx001 <zvsx001@gmail.com>
2023-09-14 01:15:05 +0300

    add ability to disable logs

--- methods/s3SyncDir.ts
+++ methods/s3SyncDir.ts
@@ -7,9 +7,10 @@ import invariant from 'invariant'
 export async function s3SyncDir(
   s3Client: Pick<S3Client, 'send'>,
   dirPath: string,
-  { bucket, bucketPath = '' }: { bucket: string; bucketPath?: string},
+  { bucket, bucketPath = '', verbose = true }: { bucket: string; bucketPath?: string; verbose?: boolean },
 ) {
   invariant(bucket, 'syncDir: bucket param is required')
+  invariant(bucketPath, 'syncDir: bucketPath param is required. In case you want to delete full bucket, prefer to use console.')
   const keys: string[] = []
 
   await recursiveWalk(dirPath, async filePath => {
@@ -27,7 +28,9 @@ export async function s3SyncDir(
       })
       await s3Client.send(command)
       keys.push(Key)
-      console.log(`Successfully uploaded ${objectPath} to s3 bucket`)
+      if (verbose) {
+        console.log(`Successfully uploaded ${objectPath} to s3 bucket`)
+      }
     } catch (error) {
       throw new Error(`error in uploading ${objectPath} to s3 bucket`)
     }
